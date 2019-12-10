7b975e059a4b6ca233e8b9b006ca51d7b6956e87
Jens-Fabian Goetzmann <mail@jefago.com>
2019-12-10 15:12:30 +0100

    First working version

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,61 @@
 
+const scrape = require('html-metadata');
+
+// Helper function to escape HTML
+const escape = (unsafe) => {
+  return (unsafe === null) ? null : 
+    unsafe.replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+}
+
+
+
 module.exports = function(config) {
+
+  config.addNunjucksAsyncFilter("linkPreview", (link, callback) => {
+    /*
+    let hash = crypto.createHash('sha1').update(link).digest('hex');
+    let file = path.join('_links', `${hash}.json`);
+    */
+    // Helper function to format links
+    const format = (metadata) => {
+      let domain = link.replace(/^http[s]?:\/\/([^\/]+).*$/i, '$1');
+      let title = escape((metadata.openGraph ? metadata.openGraph.title : null) || metadata.general.title || "");
+      let author = escape(((metadata.jsonLd && metadata.jsonLd.author) ? metadata.jsonLd.author.name : null) || "");
+      let image = escape((metadata.openGraph && metadata.openGraph.image) ? (Array.isArray(metadata.openGraph.image) ? metadata.openGraph.image[0].url : metadata.openGraph.image.url) : null);
+      let description = escape(((metadata.openGraph ? metadata.openGraph.description : "") || metadata.general.description || "").trim());
+      
+      if (description.length > 140) {
+        description = description.replace(/^(.{0,140})\s.*$/s, '$1') + '…';
+      }
+      return  `<p class="lp"><a class="lp-img" href="${link}">` +
+    //          (image ? `<img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="${image}" alt="${title}">` : '') +
+              (image ? `<img src="${image}" alt="${title}">` : '') +
+              `</a><a class="lp-met" href="${link}"><strong class="lp-ttl">${title}<br></strong><em class="lp-dsc">${description}</em>` +
+              (author ? `<span class="lp-by">${author}</span>` : ``)+
+              `<span class="lp-dom">${domain}</span></a></p>`.replace(/[\n\r]/g, ' ');
+    }
+    
+
+/*    if (fs.existsSync(file)) {
+      console.log(`[linkPreview] Using persisted data for link ${link}.`);
+      fs.readFile(file, (err, data) => {
+        if (err) callback("Reading persisted metadata failed", `<div style="color:#ff0000; font-weight:bold">ERROR: Reading persisted metadata failed</div>`);
+        callback(null, format(JSON.parse(data.toString('utf-8'))));
+      });
+    } else {
+      console.log(`[linkPreview] No persisted data for ${link}, scraping.`);*/
+      scrape(link).then((metadata => {
+        if (!metadata) callback ("No metadata", `<div style="color:#ff0000; font-weight:bold">ERROR: Did not receive metadata</div>`);
+//        fs.writeFile(file, JSON.stringify(metadata, null, 2), (err) => { /* Ignore errors, worst case we parse the link again */ });
+        callback(null, format(metadata)); 
+      }));
+  //  }  
+  });
+
 	return {
     // We process everything we know how to handle, plus images, css
 		templateFormats: [
--- index.md
+++ index.md
@@ -4,3 +4,5 @@ layout: layout.njk
 ---
 ## Rich link previews in Eleventy
 This is a demonstration of rich link previews in Eleventy
+
+{{"https://www.jefago.com/product-leadership/creating-product-roadmaps/" | linkPreview | safe}}
